d421a5f4fbc5e2ee56e9e6dab27a7ffc1daa95fe
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-04 09:42:16 -0300

    Fontes personalizadas

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
 import Groups from "@/components/Groups";
 import theme from "@/theme";
 import { ThemeProvider } from "styled-components/native";
+import { ActivityIndicator } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function Home() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
