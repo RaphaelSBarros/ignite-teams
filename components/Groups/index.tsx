2844e3f5239542d65ecce714e6f85fe31a50cc30
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-05 13:38:11 -0300

    Componente List Empty

--- components/Groups/index.tsx
+++ components/Groups/index.tsx
@@ -4,9 +4,10 @@ import Header from "../Header";
 import Highlight from "../Highlight";
 import { Container } from "./styles";
 import { FlatList } from "react-native";
+import ListEmpty from "../ListEmpty";
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -17,6 +18,10 @@ export default function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
