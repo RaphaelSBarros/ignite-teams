b994f111501910526c6aef309d7727c20ab6937d
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-05 11:36:35 -0300

    Criando Componente Highlight

--- components/Groups/index.tsx
+++ components/Groups/index.tsx
@@ -1,10 +1,12 @@
 import Header from "../Header";
+import Highlight from "../Highlight";
 import { Container } from "./styles";
 
 export default function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
