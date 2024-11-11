23c6d3d78ec4be43f93fa7e0be6b5ef289c998d4
sz-raphael-barros <raphael.barros@szsolucoes.com.br>
2024-11-11 14:06:24 -0300

    Passando parâmetros pela rota

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,29 +4,34 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group });
+  }
+
   return (
     <Container>
       <Header showBackButton />
 
       <Content>
         <Icon />
-        
-        <Highlight 
+
+        <Highlight
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input
-          placeholder="Nome da turma"
-        />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
-        <Button 
-          title="Criar"
-          style={{ marginTop: 20 }}
-        />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
-  )
+  );
 }
